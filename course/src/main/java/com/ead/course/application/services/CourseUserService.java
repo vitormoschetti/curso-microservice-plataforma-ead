2ceca5eb0086d2979fec9c76d9c05e80b0d25c5c
Vitor Moschetti <vitormoschetti@terra.com.br>
2021-12-07 10:47:32 -0300

    api composition pattern

--- course/src/main/java/com/ead/course/application/services/CourseUserService.java
+++ course/src/main/java/com/ead/course/application/services/CourseUserService.java
@@ -1,4 +1,11 @@
 package com.ead.course.application.services;
 
+import com.ead.course.application.model.UserDTO;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
 public interface CourseUserService {
+    PageImpl<UserDTO> getAllUsersByCourse(UUID userId, Pageable pageable);
 }
--- course/src/main/java/com/ead/course/application/services/impl/CourseUserServiceImpl.java
+++ course/src/main/java/com/ead/course/application/services/impl/CourseUserServiceImpl.java
@@ -1,15 +1,23 @@
 package com.ead.course.application.services.impl;
 
-import com.ead.course.adapter.repository.CourseUserRepository;
+import com.ead.course.adapter.clients.UserClient;
+import com.ead.course.application.model.UserDTO;
 import com.ead.course.application.services.CourseUserService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class CourseUserServiceImpl implements CourseUserService {
 
-    private final CourseUserRepository courseUserRepository;
+    private final UserClient userClient;
 
-
+    @Override
+    public PageImpl<UserDTO> getAllUsersByCourse(UUID courseId, Pageable pageable) {
+        return userClient.getAllUsersByCourse(courseId, pageable);
+    }
 }
